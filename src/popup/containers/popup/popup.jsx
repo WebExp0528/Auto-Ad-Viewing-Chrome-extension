328ddb2228c5c096f5c4d6df626ae9d26284212f
WebExp0528 <app.dev0528@gmail.com>
2020-05-22 10:55:08 -0700

    create project

---
+++ src/popup/containers/popup/popup.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "../../components/button/button";
+import sendMessage from "../../services/comunicationManager";
+
+function setGreen() {
+    sendMessage("change-color", { color: "green" });
+}
+
+function setRed() {
+    sendMessage("change-color", { color: "red" });
+}
+
+export default () => <div></div>;
